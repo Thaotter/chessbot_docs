d23021be7cb0f62f167f0bb858fd171c75343951
Thaotter <102908631+Thaotter@users.noreply.github.com>
2024-12-15 16:41:06 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'ChessBot'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2025, ChessBot'
+author = 'Thaotter'
 
 release = '0.1'
 version = '0.1.0'
